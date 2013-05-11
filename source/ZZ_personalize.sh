b19ca3d933f16c55eabf7df9106c698d28fba88d
Derek Flenniken <EMAIL>
2013-05-11 10:02:47 -0700

    EDITOR & VISUAL

--- source/ZZ_personalize.sh
+++ source/ZZ_personalize.sh
@@ -1,3 +1,5 @@
-export EDITOR=vim
+export EDITOR="vi -e"
+export VISUAL="vim"
+export LESSEDIT="$VISUAL %f"
 export PATH=/usr/local/share/python:$PATH
 . ~/Library/Python/2.7/lib/python/site-packages/powerline/bindings/bash/powerline.sh
